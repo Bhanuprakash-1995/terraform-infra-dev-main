642cfa4d8e750ee29776a0cbc0666947e2afcbfb
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2024-02-01 18:53:14 +0530

    first

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -1,16 +1,9 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.31.0" # AWS provider version, not terraform version
-    }
-  }
-
   backend "s3" {
-    bucket = "daws76s-state-dev"
-    key    = "vpc"
-    region = "us-east-1"
-    dynamodb_table = "daws76s-locking-dev"
+    bucket         = "roboshop-01"
+    key            = "shipping"
+    region         = "us-east-1"
+    dynamodb_table = "roboshopapp-lock"
   }
 }
 
